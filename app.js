089c7771a5baaa5229880d63649f31375b3e3b43
david daniels <ddaniels7009@gmail.com>
2021-07-03 23:56:06 -0600

    Made hover function its own function outside of the make cells function.

--- app.js
+++ app.js
@@ -16,17 +16,17 @@ createGrid(16); // Create initial grid
 function createGrid(gridSize) {
     makeRows(gridSize);
     makeCells(gridSize);
-    
+
 }
 
 // Function to create the rows for the grid
 function makeRows(rowNum) {
 
     for (r = 0; r < rowNum; r++) {
-        
+
         let row = document.createElement("div");
         container.appendChild(row);
-        
+
         row.classList.add('gridRow'); // Set class for all rows
         row.setAttribute('id', r); // Set id in order to delete grid
     }
@@ -38,41 +38,49 @@ function makeCells(cellNum) {
 
     for (c = 0; c < rows.length; c++) {
         for (j = 0; j < cellNum; j++) {
-           
+
             let cells = document.createElement("div"); // create column
             rows[j].appendChild(cells); // append column to row[j]
-    
+
             cells.classList.add('cells'); // Set class for all cells
             cells.style.width = 600 / cellNum; // Set cell width
             cells.style.height = 600 / cellNum; // Set cell height
-            cells.addEventListener('mouseenter', hoverFunction); // Add event listener to a hover funtion
+            cells.addEventListener('mouseenter', function () { hoverFunction(cells) }); // Add event listener to a hover funtion
+            cells.style.backgroundColor = 'darkslategrey';
 
-            // Create hover function for cells to change color
-            function hoverFunction() {
-                cells.style.backgroundColor = 'cadetblue';
-            }
+
         }
     }
-
 }
+// Create hover function for cells to change color
+function hoverFunction(cells) {
+
+    if (cells.style.backgroundColor == 'darkslategrey') {
+        cells.style.backgroundColor = 'cadetblue';
+    }
+}
+
+
 
 // Function to reset/create a new grid based on user input for grid size
 function resetGrid() {
-    for (let i = 0; i <= gridSize-1; i++) {
+    for (let i = 0; i <= gridSize - 1; i++) {
         container.removeChild(document.getElementById(i))
-        }
-        gridSize = window.prompt("Please enter the grid size.")
-        if(gridSize >100){
-            gridSize = 100;
-            window.alert("Maximum grid size is 100.")
-        }
-        
-        else if(gridSize < 2){
-            gridSize = 2;
-            window.alert("Minimum grid size is 2.")
-        }
+    }
+    gridSize = window.prompt("Please enter the grid size.")
+    if (gridSize > 100) {
+        gridSize = 100;
+        window.alert("Maximum grid size is 100.")
+    }
 
-        createGrid(gridSize)
+    else if (gridSize < 2) {
+        gridSize = 2;
+        window.alert("Minimum grid size is 2.")
+    }
+
+    createGrid(gridSize)
 }
+
+
 
 
